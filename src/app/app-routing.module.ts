be7424c8a2c63bedea50370726d538bfc9d64409
Ualter Junior <ualter.junior@gmail.com>
2019-04-19 12:18:42 +0200

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,23 +2,14 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'home',
-    loadChildren: './home/home.module#HomePageModule'
-  },
-  {
-    path: 'list',
-    loadChildren: './list/list.module#ListPageModule'
-  },
-  { path: 'map', loadChildren: './pages/map/map.module#MapPageModule' },
-  { path: 'setting', loadChildren: './pages/setting/setting.module#SettingPageModule' },
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+
+  { path: 'home',     loadChildren: './home/home.module#HomePageModule'},
+  { path: 'list',     loadChildren: './list/list.module#ListPageModule' },
+  { path: 'map',      loadChildren: './pages/map/map.module#MapPageModule' },
+  { path: 'setting',  loadChildren: './pages/setting/setting.module#SettingPageModule' },
   { path: 'airplane', loadChildren: './pages/airplane/airplane.module#AirplanePageModule' },
-  { path: 'tabs', loadChildren: './tabs/tabs.module#TabsPageModule' }
+  { path: 'tabs',     loadChildren: './pages/tabs/tabs.module#TabsPageModule' }
 ];
 
 @NgModule({
